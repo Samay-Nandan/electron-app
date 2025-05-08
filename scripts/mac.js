6b9e89a6ec6f72cf23bfb7840da129734bc14e47
Samay Nandan <samaynandan@gmail.com>
2025-05-08 13:30:58 +0530

    node script/mac.js file created

---
+++ scripts/mac.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+
+const allowedExtensions = [".dmg", ".zip"];
+
+const cleanDirectory = (dirPath) => {
+  if (!fs.existsSync(dirPath)) return;
+
+  const entries = fs.readdirSync(dirPath);
+  for (const entry of entries) {
+    const entryPath = path.join(dirPath, entry);
+    const stat = fs.statSync(entryPath);
+
+    if (stat.isDirectory()) {
+      fs.rmSync(entryPath, { recursive: true, force: true });
+    } else if (
+      stat.isFile() &&
+      !allowedExtensions.includes(path.extname(entry))
+    ) {
+      fs.unlinkSync(entryPath);
+    }
+  }
+};
+
+cleanDirectory(path.resolve("build", "mac"));
